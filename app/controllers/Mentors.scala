540f39f5d7587b3547857dbd13273bb0b2124281
sean <sean@sean-ThinkPad-W520.(none)>
2013-12-02 01:34:23 -0500

    added Mentors page

---
+++ app/controllers/Mentors.scala
@@ -0,0 +1,86 @@
+package controllers
+
+import play.api.mvc._
+import controllers.bootstrap.{Features, Marketing, Carousel, Featurette}
+import models.Mentor
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sean
+ * Date: 17/11/13
+ * Time: 7:32 PM
+ * To change this template use File | Settings | File Templates.
+ */
+object Mentors extends Controller {
+
+  def index = Action {
+    val carouselFeatures: List[Featurette] = List(
+      new Featurette(
+        "Mentor Students",
+        """SmarterPool allows you to be a mentor for students in university and looking to start their careers.""".stripMargin,
+        "Sign up today",
+        imageLink = routes.Assets.at("images/slide-01.jpg").toString(),
+        buttonLink = "#signup",
+        classes = "active"
+      ),
+      new Featurette(
+        "Earn Rewards",
+        """Mentors will be rewarded for their efforts through our rewards and recognition system.""".stripMargin,
+        "Sign up today",
+        imageLink = routes.Assets.at("images/slide-02.jpg").toString(),
+        buttonLink = "#signup"
+      )
+    )
+    val carousel = new Carousel(carouselFeatures)
+
+    val marketingFeatures: List[Featurette] = List(
+      new Featurette(
+        "What is SmarterPool?",
+        """SmarterPool is a an online platform that connects students with companies through real world challenges. This
+          |solution was created as a result of the high student/graduate unemployment and lack of corporate innovation
+          |happening in India. By posting challenges, companies identify students based on skills, and use a larger,
+          |smarter pool, to collaborate on innovative solutions. Students find challenges that resonate with their
+          |interests and studies, and earn temporary income for their work.
+        """.stripMargin
+      ),
+      new Featurette(
+        "Why Mentor?",
+        """The next generation of students need your help to develop the skills and the career to succeed. Help as much
+          |or as little as you want as we provide you an easy platform to do it. We also encourage mentorship by offering
+          |rewards and recognition to those that mentor successfully.
+        """.stripMargin
+      ),
+      new Featurette(
+        "",
+        "",
+        button = "See Our FAQ",
+        buttonLink = routes.Companies.faq.toString()
+      )
+    )
+    val marketing = new Marketing(marketingFeatures, 2)
+
+    val features: List[Featurette] = List(
+      new Featurette(
+        "Oh yeah, it's that good.",
+        "Sign up to view the complete list of companies that we are partnered with.",
+        tagline = "See for yourself."
+      )
+    )
+    val feature = new Features(features)
+    Ok(views.html.base("Mentors.")(carousel.Html)(marketing.Html + feature.Html + views.html.signup.student_signup.apply())("mentors"))
+  }
+
+  def signUp = Action { request =>
+    val username = request.body.asFormUrlEncoded.get("username") mkString ""
+    val password = request.body.asFormUrlEncoded.get("password") mkString ""
+    val email = request.body.asFormUrlEncoded.get("email") mkString ""
+    val field = request.body.asFormUrlEncoded.get("field") mkString ""
+    var mentor =  new Mentor(username = username, password = password, email = email, field = field)
+    var success = mentor.write()
+    if (success == None) {
+      controllers.Status.failure
+    } else {
+      controllers.Status.success
+    }
+  }
+}
